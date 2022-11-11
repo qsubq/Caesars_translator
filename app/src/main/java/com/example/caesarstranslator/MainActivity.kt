3e356e5b9d2878a6dc8c36c86ffe88f1b33516ef
qsubq <maksim-kargin@bk.ru>
2022-11-11 09:17:20 +0300

    ViewBinding

--- app/src/main/java/com/example/caesarstranslator/MainActivity.kt
+++ app/src/main/java/com/example/caesarstranslator/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.caesarstranslator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.caesarstranslator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
