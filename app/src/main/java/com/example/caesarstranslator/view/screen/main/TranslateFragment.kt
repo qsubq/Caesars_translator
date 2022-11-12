5189955673a52db5e699aabb50029788421d0fb4
qsubq <maksim-kargin@bk.ru>
2022-11-12 18:21:08 +0300

    translating fun

--- app/src/main/java/com/example/caesarstranslator/view/screen/main/TranslateFragment.kt
+++ app/src/main/java/com/example/caesarstranslator/view/screen/main/TranslateFragment.kt
@@ -1,6 +1,8 @@
 package com.example.caesarstranslator.view.screen.main
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +18,33 @@ class TranslateFragment : Fragment() {
     ): View {
         binding = FragmentTranslateBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tiEtText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.tiEtDecText.setText(cipher(p0.toString(), 3))
+            }
+        })
+    }
+
+    private fun cipher(message: String?, offset: Int): String {
+        val result = StringBuilder()
+        for (character in message!!.toCharArray()) {
+            if (character != ' ') {
+                val originalAlphabetPosition = character - 'a'
+                val newAlphabetPosition = (originalAlphabetPosition + offset) % 26
+                val newCharacter = ('a'.code + newAlphabetPosition).toChar()
+                result.append(newCharacter)
+            } else {
+                result.append(character)
+            }
+        }
+        return result.toString()
     }
 
 }
